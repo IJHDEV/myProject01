8743a5b6a15129a4a91b3dbe092d120b9beffa7d
ehdgus128 <ldh159357@naver.com>
2023-06-25 20:19:24 +0900

    2019

--- src/main/java/com/spring5/mypro01/common/filedownload/FileDownloadAjaxController.java
+++ src/main/java/com/spring5/mypro01/common/filedownload/FileDownloadAjaxController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class FileDownloadAjaxController {
 	
-	//2007
+	//2019
 	@GetMapping(value = "fileDownloadAjax",
 				produces = "application/octet-stream")
 	@ResponseBody
